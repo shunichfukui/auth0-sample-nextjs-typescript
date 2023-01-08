6298ec05b4df326c8bc658361ae97bf5d9e4ce9c
shunich <fukusyun3@gmail.com>
2023-01-08 10:39:39 +0900

    add: auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Auth0Provider } from '@auth0/auth0-react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return(
+    <Auth0Provider
+      domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]}
+      clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]}
+      redirectUri={process.env["NEXT_PUBLIC_BASE_URL"]}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
+
 }
+export default MyApp
